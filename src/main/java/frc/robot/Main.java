93fc36a6035192a805c53c784e7f1208e725e7f0
Russc243 <Russc243@optonline.net>
2020-01-26 18:07:33 -0500

    test github log out

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-//test comment
+//test comment 2
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
